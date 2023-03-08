9201819a4655d2ad1b5d6759ce90d44faaca9a1b
mjorrico <jordanenrico214@gmail.com>
2023-03-08 23:08:35 +0000

    working multi-cluster HDFS

--- start-worker.sh
+++ start-worker.sh
@@ -23,19 +23,22 @@ fi
 img="hadooptest"
 net="spark-network"
 vol="hdfs-volume"
+master_address="192.168.2.208"
 
 # ports="-p 7077:7077 -p 8080:8080 -p 9000:9000 -p 9870:9870 -p 4040-4050:4040-4050"
 network="--network $net"
 volume="-v $vol:/hdfs-data"
 hostname="--hostname $host"
 name="--name $host"
-run="run $ports $network $volume $hostname $name $img"
+run="run $network $volume $hostname $name $img"
 
-start_namenode="./hadoop-3.3.4/bin/hdfs --daemon start datanode"
+edithost="echo $master_address master-node >> /etc/hosts"
+start_datanode="hdfs --daemon start datanode"
 jps="jps"
 sleep="sleep 999999d"
-add_master="echo 192.168.2.208 master-node >> /etc/hosts"
-command="$start_namenode && $jps && $add_master && $sleep"
+command="$start_datanode && $jps && $sleep"
 
+echo ">>> docker network create $net"
 docker network create $net
+echo -e ">>> docker $run sh -c \"$command\""
 docker $run sh -c "$command"
