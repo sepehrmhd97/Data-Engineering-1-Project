fea466f613d3c07dfe143ef1cf6ae921875f8a5f
mjorrico <jordanenrico214@gmail.com>
2023-03-09 17:36:02 +0000

    now using host network

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ ENV SPARK_HOME="/usr/local/spark"
 ENV SPARK_NO_DAEMONIZE="true"
 ENV JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
 ENV PATH="${PATH}:$SPARK_HOME/bin:$HADOOP_HOME/bin"
+ENV HDFS_NAMENODE_USER="root"
+ENV HDFS_DATANODE_USER="root"
+ENV HDFS_SECONDARYNAMENODE_USER="root"
+ENV YARN_RESOURCEMANAGER_USER="root"
+ENV YARN_NODEMANAGER_USER="root"
+
 RUN apt-get update
 RUN apt-get -y upgrade
 RUN apt-get install -y net-tools
@@ -16,6 +22,7 @@ RUN tar -xvf hadoop-3.3.4.tar.gz
 RUN rm hadoop-3.3.4.tar.gz
 COPY hadoop-configs/core-site.xml /hadoop-3.3.4/etc/hadoop/
 COPY hadoop-configs/hdfs-site.xml /hadoop-3.3.4/etc/hadoop/
+COPY hadoop-configs/workers /hadoop-3.3.4/etc/hadoop/
 
 EXPOSE 7077
 EXPOSE 8080
--- start-master.sh
+++ start-master.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 img="hadooptest"
-net="spark-network"
 host="master-node"
 vol="hdfs-volume"
 
-ports="-p 7077:7077 -p 8080:8080 -p 9000:9000 -p 9870:9870 -p 4040-4050:4040-4050"
-network="--network $net"
+network="--network host"
 volume="-v $vol:/hdfs-data"
 hostname="--hostname $host"
 name="--name $host"
-run="run $ports $network $volume $hostname $name $img"
+run="run $network $volume $hostname $name $img"
 
 format_namenode="yes y | hdfs namenode -format"
 start_namenode="hdfs --daemon start namenode"
@@ -29,7 +27,5 @@ while getopts "f" opt; do
   esac
 done
 
-echo ">>> docker network create $net"
-docker network create $net
 echo -e ">>> docker $run sh -c \"$command\""
 docker $run sh -c "$command"
