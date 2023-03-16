1022d67697d03fb0397e5ea561cc1cfb8cd4d795
mjorrico <jordanenrico214@gmail.com>
2023-03-16 15:53:57 +0000

    attempting to fix JAVA_HOME in env.sh files

--- hadoop-configs/hadoop-env.sh
+++ hadoop-configs/hadoop-env.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
--- hadoop-configs/spark-env.sh
+++ hadoop-configs/spark-env.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+
 SPARK_MASTER_HOST='192.168.2.208'
+JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/"
